3e442119f7da14d9719d63126bec7f3e648c21bd
Sparshlohana <sparsh.lohana@gmail.com>
2024-09-12 01:08:54 +0530

    python chapter 1 completed"

---
+++ Python/01_introduction/01_introduction.py
@@ -0,0 +1,67 @@
+# According to stack overflow, Python is the most loved and easiest programming language to learn.
+
+# Prerequisites for learning Python:
+# There is no need to learn any other programming language before learning Python.
+# Even if python is your first programming language, it is totally fine.
+
+# So first things first, what is a programming language?
+# Just like we use Hindi or English or any other language to communicate with each other, we use programming language like Python to communicate with the computer.
+# So if we want to tell the computer to do something, we need to write the instructions in a programming language.
+# Eg. If we want to sum two numbers, we can't verbally tell the computer to do so. We need to write a program using a programming language to do so.
+# Just like if you are talking to a french person, you need to speak in French, if you are speaking to a chinese person, you need to speak in Chinese. Similarly, if you are talking to a computer, you need to speak in a programming languages like python, c, c++, java, rust, ruby, golang, javascript etc.
+
+# Why can python be your first programming language?
+# This is because Python is simple and easy to learn and understand.
+# It feels like you are reading simple english language. It truly feels like you are reading plain and simple english language. This is an opinion of many people who have learned python as their first programming language.
+
+# Features of Python:
+# 1. Easy to learn and understand.
+# 2. Development time is less.
+# 3. Supports AI, ML, Data Science, Web Development, Automation, Scripting, Game Development, Desktop Applications, Mobile Applications etc. 
+# 4. Various libraries and frameworks are available.
+# 5. Open Source.
+# 6. Free to use.
+# 7. High level language.
+
+# What is a high level language?
+# High level language is a language which is easy to understand and write. It is closer to human language than machine language.
+# Machine language is a language which is understood by the computer. It is a binary language. It is difficult to understand and write.
+# High level language is converted to machine language using a compiler or interpreter.
+
+# Hardware devices are those devices which are physical and can be touched. Eg. CPU, RAM, GPU.
+# Software devices are those devices which are virtual and can't be touched and which are executed on hardware devices. Eg. Operating System, Programs, Applications etc.
+
+# Python is a software device. It is a programming language. It is executed on hardware devices like CPU, RAM etc. It will instruct the hardware devices to do something.
+
+# Basically, The code we write in python is gone through a python interpreter which converts the code to machine language and then the hardware devices execute the machine language code.
+# You cannot directly talk to hardware, you need to talk to the hardware through software. Python is a software which will talk to the hardware on your behalf.
+
+# 8. Its fun to work with Python.
+# 9. Its portable. (Can be run on any platform like Windows, Mac, Linux etc.)
+
+
+# lets install python and run our first program.
+# Go to https://www.python.org/downloads/ and download the latest version of python.
+# Click on the downloaded file which you just downloaded
+# A screen will open, click on "Add Python 3.x to PATH" and click on "Install Now"
+# It may ask for your permission, click on "Yes"
+# It may take some time to install python.
+
+# We will use an IDE (Integrated Development Environment) to write and run our python code.
+# IDE is a software which is used to write and run code.
+# We will use Visual Studio Code as our IDE.
+# VS Code is a free and open source IDE. You can download it from https://code.visualstudio.com/download and install it.
+# Again it may ask for your permission, click on "Yes"
+# Click on all the tick marks and click on "Next"
+# Click on "Install"
+# Click on "Finish"
+
+# In VS Code, go to settings section or just press "Ctrl + ," and search for mouse wheel zoom.
+# Check the box which says "Editor: Mouse Wheel Zoom". This will allow you to zoom in and out using your mouse wheel. Just press "Ctrl" and scroll your mouse wheel to zoom in and out.
+
+# Go to extensions section or just press "Ctrl + Shift + X" and search for python.
+# Click on the first result and click on "Install"
+# Click on "Reload" to reload the window.
+
+# Congratulations, you have successfully installed python and VS Code.
+# Now lets write our first python program in next chapter.
