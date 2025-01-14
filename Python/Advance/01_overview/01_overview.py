813e3b6a873d0d675c072a667781fa5d915da4f5
Sparshlohana <sparsh.lohana@gmail.com>
2025-01-14 22:51:47 +0530

    advance python chap 1 completed

---
+++ Python/Advance/01_overview/01_overview.py
@@ -0,0 +1,19 @@
+# In this advance python, we will learn about how python works behind the scenes and its architecture.
+
+# Just like other programming languages, python also has its own architecture. It has several key components that work together to execute the code.
+
+# Why to learn python architecture?
+# It is important to understand the architecture of python to write efficient code and to debug the code easily.
+# You'll be able to write optimized code!
+# Debugging complex issues will become easier and you will be able to understand the root cause of the issue.
+# You'll gain a deeper appreciation of how Python translates your thoughts into machine actions.
+
+# If you learn any language, you should know how it works behind the scenes. It will clear all your concepts and you will be able to write efficient code.
+
+# Basically this advance python will help you clear your basics! How ironic it is!
+
+# There are many components in python architecture like source code, interpreter, compiler, standard libraries, etc. We will be discussing them one by one in detail.
+
+# I am sorry to tell you that this whole advance python is theoretical and it will take a lot of time to understand it. So, be patient and read it carefully.
+
+# There are many deep concepts in this advance python. So, I will try to explain them in the simplest way possible. So, be with me and give your best!
